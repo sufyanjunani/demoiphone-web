aa348026f8b8ea85930d2d64d9295294e91c762e
Sufyanjunani <sasifjunani@gmail.com>
2024-02-23 02:19:49 +0500

    add my assignment

---
+++ app.js
@@ -0,0 +1,46 @@
+// chapter 01
+
+alert("Q#1 \n Error! Please enter a valid password.")
+
+alert("Q#2 \n Welcome to JS Land...\n Happy Coding!")
+
+alert("Q#3 \n Welcome to JS Land...")
+
+alert("Q#4 \n Happy Coding! \n Prevent this page from creating additional dialogs.")
+
+alert("Q#5 \n Hello... I can run JS through my web browser's console")
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// chapter 02
+
+var username= "Jhone Doe"
+
+var age= "15 years old"
+
+var site= "14 times"
+
+var notification= "Certified Mobile Application Devevlopment"
+
+var message= "HELLO WORLD \n IM LEARNING FROM A SMARTER WAY TO LEARN JAVASCRIPT ES5"
+
+var email= "example@example.com"
+
+var shirt= "5 T-shirt"
+
+alert ("Q#1 \n"+ username )
+alert ("Q#2 \n"+ age)
+alert ("Q#3 \n"+ notification)
+alert ("Q#4 \n"+ message)
+alert ("Q#5 \n My email address is "+  email)
+alert ("Q#6 \n I am trying to learn from "+ message)
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// chapter 03
+
+alert ("Q#1 \n I am "+ age)
+alert ("Q#2 \n you have visited this site "+ site)
+alert ("Hello check browser")
+document.write(username+"ordered"+shirt+"(s) on XYZ Clothing Store")
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
